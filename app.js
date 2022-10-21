3c66b74d81620a01c9cbc311a0b560e501cbf842
Meldin Radoncic <meldinradoncic89@gmail.com>
2022-10-20 20:27:52 -0400

    changing the path of public folder

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ const jobsRoutes = require("./routes/jobs");
 const morgan = require("morgan");
 
 const app = express();
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 
 app.use(cors());
